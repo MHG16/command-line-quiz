8b6ea52df0ee16f87ccb308e037d3f9ea5c1cd7e
Martin Grossmann <mhgrossmann@yahoo.com>
2016-03-08 16:36:31 -0600

    Part 1 of adventurer mode complete

--- scripts/main.js
+++ scripts/main.js
@@ -6,109 +6,185 @@ var percentCorrect = 0;
 
 
 	var answer = prompt('What does HTML stand for?');
-	if (answer === 'hypertext markup language') {
+	var correct = 'hypertext markup language';
+	if (answer === correct) {
+		alert('That is correct!');
 		answeredCorrect += 1;  
-	}	
+	} else {
+		alert('That is incorrect. The correct answer is: '+correct);	
+	}
 
 	currentQuestion += 1;
 
 	var answer = prompt('What is a <p> tag used for?');
-	if (answer === 'paragraph') {
+	var correct = 'paragraph';
+	if (answer === correct) {
+		alert('That is correct!');
 		answeredCorrect += 1;  
 	
-	}	
+	} else {	
+	alert('That is incorrect. The correct answer is: '+correct);
+	}
+
 	currentQuestion += 1; 
 
 	var answer = prompt('What comes first on your html page? Is it <!DOCTYPE = html>?');
-	if (answer === 'yes') {
+	var correct = 'yes';
+	if (answer === correct) {
+		alert('That is correct!');
 		answeredCorrect += 1;  
 	
-	}	
+	} else {	
+	alert('That is incorrect. The correct answer is: '+correct);
+	}
+
 	currentQuestion += 1;
 
 	var answer = prompt('What is the space between two boxes?');
-	if (answer === 'margin') {
+	var correct = 'margin';
+	if (answer === correct) {
+		alert('That is correct!');
 		answeredCorrect += 1;  
 	
-	}	
+	} else {	
+	alert('That is incorrect. The correct answer is: '+correct);
+	}
+
 	currentQuestion += 1;
 
 	var answer = prompt('How do you center a text element? Is it text-align: center; ?');
-	if (answer === 'yes') {
+	var correct = 'yes';
+	if (answer === correct) {
+		alert('That is correct!');
 		answeredCorrect += 1;  
 	
-	}	
+	} else {	
+	alert('That is incorrect. The correct answer is: '+correct);
+	}
+
 	currentQuestion += 1;
 
 	var answer = prompt('Is parseInt() used to convert a string to a number?');
-	if (answer === 'no') {
+	var correct = 'no';
+	if (answer === correct) {
+		alert('That is correct!');
 		answeredCorrect += 1;  
 	
-	}	
+	} else {	
+	alert('That is incorrect. The correct answer is: '+correct);
+	}
+
 	currentQuestion += 1;
 
 	var answer = prompt('Does the content in a Block DIV fill its container?');
-	if (answer === 'yes') {
+	var correct = 'yes';
+	if (answer === correct) {
+		alert('That is correct!');
 		answeredCorrect += 1;  
 	
-	}	
+	} else {	
+	alert('That is incorrect. The correct answer is: '+correct);
+	}
+
 	currentQuestion += 1;
 
 	var answer = prompt('What it the tag for list item ?');
-	if (answer === '<li>') {
+	var correct = '<li>';
+	if (answer === correct) {
+		alert('That is correct!');
 		answeredCorrect += 1;  
 	
-	}	
+	} else {	
+	alert('That is incorrect. The correct answer is: '+correct);
+	}
+
 	currentQuestion += 1;
 
 	var answer = prompt('How do you get rid of the underline on a link?');
-	if (answer === 'text-decoration: none') {
+	var correct = 'text-decoration: none';
+	if (answer === correct) {
+		alert('That is correct!');
 		answeredCorrect += 1;  
 	
-	}	
+	} else {	
+	alert('That is incorrect. The correct answer is: '+correct);
+	}
+
 	currentQuestion += 1;
 
 	var answer = prompt('What do we use to apply different css rules depending on different screen sizes?');
-	if (answer === 'media queries') {
+	var correct = 'media queries';
+	if (answer === correct) {
+		alert('That is correct!');
 		answeredCorrect += 1;  
 	
-	}	
+	} else {	
+	alert('That is incorrect. The correct answer is: '+correct);
+	}
+
 	currentQuestion += 1;
 
 	var answer = prompt('What feature of Sass allows us to logically chunk code to make it more readable?');
-	if (answer === 'nesting') {
+	var correct = 'nesting';
+	if (answer === correct) {
+		alert('That is correct!');
 		answeredCorrect += 1;  
 	
-	}	
+	} else {	
+	alert('That is incorrect. The correct answer is: '+correct);
+	}
+
 	currentQuestion += 1;
 
 	var answer = prompt('Are true and false Booleans?');
-	if (answer === 'yes') {
+	var correct = 'yes';
+	if (answer === correct) {
+		alert('That is correct!');
 		answeredCorrect += 1;  
 	
-	}	
+	} else {	
+	alert('That is incorrect. The correct answer is: '+correct);
+	}
+
 	currentQuestion += 1;
 
 	var answer = prompt('Does Nan have a truthy value ?');
-	if (answer === 'no') {
+	var correct = 'no';
+	if (answer === correct) {
+		alert('That is correct!');
 		answeredCorrect += 1;  
 	
-	}	
+	} else {	
+	alert('That is incorrect. The correct answer is: '+correct);
+	}
+
 	currentQuestion += 1;
 
 	var answer = prompt('How does JavaScript represent Boolean and ?');
-	if (answer ='&&') {
+	var correct = '&&';
+	if (answer = correct) {
+		alert('That is correct!');
 		answeredCorrect += 1;  
 	
-	}	
+	} else {	
+	alert('That is incorrect. The correct answer is: '+correct);
+	}
+
 	currentQuestion += 1;
 
-	var answer = prompt('Is #picture an example of a class ?');
-	if (answer === 'no') {
+	var answer = prompt('Is #picture an example of a css class ?');
+	var correct = 'no';
+	if (answer === correct) {
+		alert('That is correct!');
 		answeredCorrect += 1;  
 	
-	}	
+	} else {
+	alert('That is incorrect. The correct answer is: '+correct);
+	}
+
 	currentQuestion += 1;
+
+
 
 
 percentCorrect = (answeredCorrect / totalQuestions) * 100 ;
